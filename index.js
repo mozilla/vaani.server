b99220ccbb61bd4dee5cad5e2969bd9d89ba7bcc
Tilman Kamp <tilmankamp@posteo.de>
2016-06-21 17:27:18 +0200

    Kaldi integration

--- index.js
+++ index.js
@@ -1,32 +1,68 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
- 
+
 'use strict';
 
 const http = require('http');
-const WebSocketServer = require('ws').Server;
+const WebSocket = require('ws');
+const WebSocketServer = WebSocket.Server;
 
 var clients = [],
     count = 0;
 
 module.exports = {
 
-    serve: function(port) {
+    serve: (port) => {
         var wss = new WebSocketServer({
             port: port
         });
-        wss.on('connection', function(ws) {
+        wss.on('connection', (ws) => {
             console.log('Connecting a client...');
-            var id = count++;
+
+            var id = count++,
+                buffer = [],
+                ttt;
             clients[id] = ws;
-            ws.on('message', function(data, flags) {
-                console.log('From client: ' + data);
-                ws.send('<A test message from the server>');
+            ws.on('message', (data, flags) => {
+                console.log('From client - type: ' + typeof(data) + ' length: ' + data.length);
+                buffer.push(data);
             });
-            ws.on('close', function() {
+            ws.on('close', () => {
                 delete clients[id];
+                clearInterval(interval);
                 console.log('Client disconnected.');
+            });
+
+            var kaldi = new WebSocket('ws://52.37.26.79:8888/client/ws/speech?content-type=audio/x-raw,layout=(string)interleaved,rate=(int)16000,format=(string)S16LE,channels=(int)1');
+            kaldi.on('open', () => {
+                console.time('kaldi');
+                console.log('connected with kaldi - starting time measurement');
+
+                var send = () => {
+                    var i;
+                    for(i = 0; i<buffer.length; i++) {
+                        kaldi.send(buffer[i]);
+                        console.log ('sent one package to kaldi');
+                    }
+                    buffer = [];
+                };
+                send();
+                var interval = setInterval(send, 250);
+            });
+            kaldi.on('message', (data, flags) => {
+                console.log ('received one package from kaldi');
+                var message = JSON.parse(data);
+                if (message.status > 0) {
+                    console.timeEnd('kaldi');
+                    ws.send(JSON.stringify({ status: message.status, message: 'Speech recognition failed', data: message }));
+                    return;
+                }
+                var result = message.result;
+                if (result && result.final) {
+                    console.timeEnd('kaldi');
+                    ws.send(JSON.stringify({ status: message.status, message: 'OK', data: result.hypotheses[0].transcript }));
+                }
             });
         });
     }
--- test/tests.js
+++ test/tests.js
@@ -2,15 +2,23 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+'use strict';
+
 const vaani = require('../index');
 const WebSocket = require('ws');
+const fs = require('fs');
 
 vaani.serve(8080);
 var ws = new WebSocket('ws://localhost:8080');
-ws.on('open', function() {
-    ws.send('<A test message from the client>');
+ws.on('open', () => {
+    fs.readFile('test/resources/helloworld.raw', (err, data) => {
+        if (err) throw err;
+        ws.send(data);
+        ws.send('EOS');
+    });
 });
-ws.on('message', function(data, flags) {
-    console.log('From server: ' + data);
-    process.exit();
+ws.on('message', (data, flags) => {
+    data = JSON.parse(data);
+    console.log(data.data);
+    process.exit(data.status);
 });
