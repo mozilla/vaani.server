7c7f36589a5b92289ae588fc1eb238aa254bd6d1
Tilman Kamp <tilmankamp@posteo.de>
2016-07-11 18:00:56 +0200

    Client certificates

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 
 'use strict';
 
+(function () {
+
 const fs = require('fs');
 const url = require('url');
 const http = require('http');
@@ -15,7 +17,6 @@ const evernote = require('./lib/evernote');
 const watson = require('watson-developer-cloud');
 const shoppinglistloader = require('./resources/shoppinglistloader');
 
-
 const sorryUnderstand = 'Sorry, but I did not quite understand.';
 const sorryService = 'Sorry, the service is not available at the moment.';
 const unknown = '<unknown>';
@@ -25,171 +26,188 @@ const ERROR_PARSING = 1;
 const ERROR_EXECUTING = 2;
 const ERROR_STT = 100;
 
-var getConfig = () => {
-        var config = JSON.parse(process.env.VAANI_CONFIG || fs.readFileSync("config.json"));
-        config.port = config.port || 80;
-        return config;
-    };
+const ssldir = './resources/ssl/';
 
-module.exports = {
+const getConfig = () => {
+    var config = JSON.parse(process.env.VAANI_CONFIG || fs.readFileSync("config.json"));
+    config.secure = !!config.secure;
+    config.port = config.port || (config.secure ? 443 : 80);
+    return config;
+};
 
-    getConfig: getConfig,
+const serve = (config, callback) => {
+    shoppinglistloader.load((parser) => {
+        config = config || getConfig();
 
-    serve: (config, callback) => {
-        shoppinglistloader.load((parser) => {
-            config = config || getConfig();
+        var server;
+        if(config.secure) {
+            server = https.createServer({
+                key:  fs.readFileSync(ssldir + 'server-key.pem'),
+                cert: fs.readFileSync(ssldir + 'server-crt.pem'),
+                ca:   fs.readFileSync(ssldir + 'ca-crt.pem'),
+                passphrase: config.passphrase,
+                requestCert: true,
+                rejectUnauthorized: true
+            });
+        } else {
+            server = http.createServer();
+        }
 
-            var server;
-            if(config.secure) {
-                const key = fs.readFileSync('./resources/key.pem', 'utf8');
-                const cert = fs.readFileSync('./resources/cert.pem', 'utf8');
-                server = https.createServer({key: key, cert: cert});
+        server.on('error', (error) => {
+            console.log('Server problem: ' + error.message);
+            process.exit(1);
+        });
+
+        const app = express();
+        app.use((req, res) => {
+            if (req.client.authorized) {
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.end('{ "status": "approved" }');
+                console.log('approved');
             } else {
-                server = http.createServer();
+                res.writeHead(401, { 'Content-Type': 'application/json' });
+                res.end('{ "status": "denied" }');
+                console.log('declined');
             }
+        });
+        server.on('request', app);
 
-            server.on('error', (error) => {
-                console.log('Server problem: ' + error.message);
-                process.exit(1);
+        server.listen(process.env.PORT || config.port || (config.secure ? 443 : 80));
+
+        const wss = new WebSocketServer({
+            server: server
+        });
+
+        const text_to_speech = watson.text_to_speech({
+            username: config.watsontts.username,
+            password: config.watsontts.password,
+            version: 'v1'
+        });
+
+        wss.on('connection', (client) => {
+
+            var buffer = [],
+                query = url.parse(client.upgradeReq.url, true).query,
+                interval,
+                kaldi = new WebSocket(
+                    config.kaldi.url +
+                    '?content-type=audio/x-raw,layout=(string)interleaved,rate=(int)16000,format=(string)S16LE,channels=(int)1'
+                );
+
+            const fail = (message) => {
+                kaldi.close();
+                client.close();
+                console.log('Failed: ' + message);
+            };
+
+            const answer = (status, message, command, confidence) => {
+                console.log('Sending answer: ' + status + ' - ' + message);
+                try {
+                    client.send(JSON.stringify({
+                        status: status,
+                        message: message,
+                        command: command,
+                        confidence: confidence || 1
+                    }));
+
+                    var voice = text_to_speech.synthesize({
+                        text: [
+                            '<express-as type="',
+                                (status > 0 ? 'Apology' : ''),
+                            '">',
+                            message,
+                            '</express-as>'
+                        ].join(''),
+                        voice: 'en-US_AllisonVoice',
+                        accept: 'audio/wav'
+                    });
+                    voice.on('data', (data) => client.send(data));
+                    voice.on('end', () => client.close());
+                } catch(ex) {
+                    fail('answering');
+                }
+            };
+
+            const interpret = (command, confidence) => {
+                var product;
+                try {
+                    product = parser.parse(command);
+                } catch (ex) {
+                    console.log('Problem interpreting: ' + command);
+                    answer(ERROR_PARSING, sorryUnderstand, command, confidence);
+                    return;
+                }
+                evernote.addNoteItem(query.authtoken, product);
+                answer(
+                    OK,
+                    'Added ' + product + ' to your shopping list.',
+                    command,
+                    confidence
+                );
+            };
+
+            client.on('message', (data, flags) => {
+                buffer.push(data);
+            });
+            client.on('error', (error) => {
+                fail('client connection')
+            });
+            client.on('close', () => {
+                clearInterval(interval);
             });
 
-            const app = express();
-            app.use((req, res) => {
-                res.send({ msg: "hello" });
-            });
-            server.on('request', app);
+            const kaldiProblem = (status) => {
+                clearInterval(interval);
+                kaldi.close();
+                answer(
+                    ERROR_STT + (status ? status : 0),
+                    sorryService,
+                    unknown
+                );
+            };
 
-            server.listen(process.env.PORT || config.port || (config.secure ? 443 : 80));
-
-            const wss = new WebSocketServer({
-                server: server
-            });
-
-            const text_to_speech = watson.text_to_speech({
-                username: config.watsontts.username,
-                password: config.watsontts.password,
-                version: 'v1'
-            });
-
-            wss.on('connection', (client) => {
-
-                var buffer = [],
-                    query = url.parse(client.upgradeReq.url, true).query,
-                    interval,
-                    kaldi = new WebSocket(
-                        config.kaldi.url +
-                        '?content-type=audio/x-raw,layout=(string)interleaved,rate=(int)16000,format=(string)S16LE,channels=(int)1'
-                    );
-
-                const fail = (message) => {
-                    kaldi.close();
-                    client.close();
-                    console.log('Failed: ' + message);
-                };
-
-                const answer = (status, message, command, confidence) => {
-                    console.log('Sending answer: ' + status + ' - ' + message);
+            kaldi.on('open', () => {
+                //console.time('kaldi time');
+                var send = () => {
                     try {
-                        client.send(JSON.stringify({
-                            status: status,
-                            message: message,
-                            command: command,
-                            confidence: confidence || 1
-                        }));
-                        var voice = text_to_speech.synthesize({
-                            text: [
-                                '<express-as type="',
-                                    (status > 0 ? 'Apology' : ''),
-                                '">',
-                                message,
-                                '</express-as>'
-                            ].join(''),
-                            voice: 'en-US_AllisonVoice',
-                            accept: 'audio/wav'
-                        });
-                        voice.on('data', (data) => client.send(data));
-                        voice.on('end', () => client.close());
+                        var i;
+                        for(i = 0; i<buffer.length; i++) {
+                            kaldi.send(buffer[i]);
+                        }
+                        buffer = [];
                     } catch(ex) {
-                        fail('answering');
-                    }
-                };
-
-                const interpret = (command, confidence) => {
-                    var product;
-                    try {
-                        product = parser.parse(command);
-                    } catch (ex) {
-                        console.log('Problem interpreting: ' + command);
-                        answer(ERROR_PARSING, sorryUnderstand, command, confidence);
-                        return;
-                    }
-                    evernote.addNoteItem(query.authtoken, product);
-                    answer(
-                        OK,
-                        'Added ' + product + ' to your shopping list.',
-                        command,
-                        confidence
-                    );
-                };
-
-                client.on('message', (data, flags) => {
-                    buffer.push(data);
-                });
-                client.on('error', (error) => {
-                    fail('client connection')
-                });
-                client.on('close', () => {
-                    clearInterval(interval);
-                });
-
-                const kaldiProblem = (status) => {
-                    clearInterval(interval);
-                    kaldi.close();
-                    answer(
-                        ERROR_STT + (status ? status : 0),
-                        sorryService,
-                        unknown
-                    );
-                };
-
-                kaldi.on('open', () => {
-                    //console.time('kaldi time');
-                    var send = () => {
-                        try {
-                            var i;
-                            for(i = 0; i<buffer.length; i++) {
-                                kaldi.send(buffer[i]);
-                            }
-                            buffer = [];
-                        } catch(ex) {
-                            kaldiProblem();
-                        }
-                    };
-                    send();
-                    interval = setInterval(send, 250);
-                });
-                kaldi.on('message', (data, flags) => {
-                    try {
-                        var message = JSON.parse(data);
-                        if (message.status > 0) {
-                            //console.timeEnd('kaldi time');
-                            kaldiProblem(message.status);
-                            return;
-                        }
-                        var result = message.result;
-                        if (result && result.final) {
-                            //console.timeEnd('kaldi time');
-                            var hypothesis = result.hypotheses[0];
-                            interpret(hypothesis.transcript, hypothesis.confidence);
-                        }
-                    } catch (ex) {
                         kaldiProblem();
                     }
-                });
-                kaldi.on('error', kaldiProblem);
+                };
+                send();
+                interval = setInterval(send, 250);
             });
-
-            callback && callback();
+            kaldi.on('message', (data, flags) => {
+                try {
+                    var message = JSON.parse(data);
+                    if (message.status > 0) {
+                        //console.timeEnd('kaldi time');
+                        kaldiProblem(message.status);
+                        return;
+                    }
+                    var result = message.result;
+                    if (result && result.final) {
+                        //console.timeEnd('kaldi time');
+                        var hypothesis = result.hypotheses[0];
+                        interpret(hypothesis.transcript, hypothesis.confidence);
+                    }
+                } catch (ex) {
+                    kaldiProblem();
+                }
+            });
+            kaldi.on('error', kaldiProblem);
         });
-    }
+
+        callback && callback();
+    });
 };
+
+exports.getConfig = getConfig;
+exports.serve = serve;
+
+})();
--- resources/createcertificates.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-if [ ! -f key.pem ] && [ ! -f cert.pem ]; then
-    echo "### Generating self-signed certificates"
-    openssl req -x509 -newkey rsa:2048 -keyout key.pem -out cert.pem -days 10000 -nodes -subj "/C=DE/ST=Berlin/L=Berlin/O=Mozilla/OU=CD/CN=mozilla.com"
-fi
